52e434176409fb12b987c4d38f1887bb1285705e
Eric Beyer <eric@Erics-MacBook-Pro.local>
2014-10-05 21:07:46 -0400

    First commit

---
+++ Assets/Card.shader
@@ -0,0 +1,28 @@
+﻿//http://docs.unity3d.com/Manual/SL-SurfaceShaderExamples.html
+Shader "Eric/Card" {
+    Properties {
+      _CardTemplateTex ("Card Template Tex", 2D) = "white" {}
+      _CardTex ("Card Tex", 2D) = "white" {}
+    }
+    SubShader {
+      Tags { "RenderType" = "Opaque" }
+      CGPROGRAM
+      #pragma surface surf Lambert
+      struct Input {
+          float2 uv_CardTemplateTex;
+          float2 uv_CardTex;
+      };
+      sampler2D _CardTemplateTex;
+      sampler2D _CardTex;
+      
+      void surf (Input IN, inout SurfaceOutput o) {
+      
+          float4 templateAlbedo = tex2D (_CardTemplateTex, IN.uv_CardTemplateTex).rgba;
+          float templateAlpha = templateAlbedo.a;
+          float3 cardAlbedo = tex2D (_CardTex, IN.uv_CardTemplateTex).rgb;
+          o.Albedo = lerp(cardAlbedo, templateAlbedo.rgb, templateAlpha);
+      }
+      ENDCG
+    } 
+    Fallback "Diffuse"
+  }
---
+++ Assets/CardScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardScript : MonoBehaviour {
+
+	private bool isCardFlipped;
+	private float initialZPosition;
+	private Vector3 startingRotation;
+	public Vector3 targetRotation;
+	private float _rotationSpeed = 7f;
+
+	// Use this for initialization
+	void Start () {
+		initialZPosition = this.transform.position.z;
+		startingRotation = transform.rotation.eulerAngles;
+		targetRotation = transform.rotation.eulerAngles;
+	}
+
+	void Update() {
+		// takes me from current to destination
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetRotation), Time.deltaTime * _rotationSpeed);
+	}
+
+	void OnMouseUp() {
+		if (!isCardFlipped) {
+			startingRotation = transform.rotation.eulerAngles;
+
+			targetRotation = new Vector3(270f, 180f, transform.rotation.z);
+
+			transform.position = new Vector3(transform.position.x, transform.position.y, 0.2f);
+
+			isCardFlipped = true;
+		}
+		else {
+			startingRotation = transform.rotation.eulerAngles;
+
+			targetRotation = new Vector3(270f, 0f, transform.rotation.z);
+
+			transform.position = new Vector3(transform.position.x, transform.position.y, initialZPosition);
+
+			isCardFlipped = false;
+		}
+	}
+}
